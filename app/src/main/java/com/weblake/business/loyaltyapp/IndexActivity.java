c2eecc62237b5391e5d3b0fb3fede1eeb48acd17
essalhi <khalid.essalhi@gmail.com>
2017-03-31 18:38:09 +0100

    interface authentification version 2

--- app/src/main/java/com/weblake/business/loyaltyapp/IndexActivity.java
+++ app/src/main/java/com/weblake/business/loyaltyapp/IndexActivity.java
@@ -1,21 +1,168 @@
 package com.weblake.business.loyaltyapp;
 
+import android.media.Image;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import org.json.JSONException;
+import org.json.JSONObject;
 
-public class IndexActivity extends AppCompatActivity {
+import android.app.Activity;
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import com.loopj.android.http.AsyncHttpClient;
+import com.loopj.android.http.AsyncHttpResponseHandler;
+import com.loopj.android.http.RequestParams;
+
+import cz.msebera.android.httpclient.Header;
+
+/**
+ *
+ * Register Activity Class
+ */
+public class IndexActivity extends Activity {
+    // Progress Dialog Object
+    ProgressDialog prgDialog;
+    // Email Edit View Object
+    EditText emailET;
+    //Validation Button
+    ImageButton validatiIB;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_index);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+       /* // Find Email Edit View control by ID
+        emailET = (EditText)findViewById(R.id.);
+        // Instantiate Progress Dialog object
+        prgDialog = new ProgressDialog(this);
+        // Set Progress Dialog Text
+        prgDialog.setMessage("Please wait...");
+        // Set Cancelable as False
+        prgDialog.setCancelable(false);
+        //
+        validatiIB = (ImageButton)findViewById(R.id.indexValidate);
+        //
+        validatiIB.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                registerUser();
+            }
+        });*/
+
     }
 
+
+    public void registerUser(){
+        // Get Email ET control value
+        String email = emailET.getText().toString();
+        // Instantiate Http Request Param Object
+        RequestParams params = new RequestParams();
+        // When Name Edit View, Email Edit View and Password Edit View have values other than Null
+        if(Utility.isNotNull(email)){
+            // When Email entered is Valid
+            if(Utility.validate(email)){
+                // Put Http parameter username with value of Email Edit View control
+                params.put("username", email);
+                // Invoke RESTful Web Service with Http parameters
+                invokeWS(params);
+            }
+            // When Email is invalid
+            else{
+                Toast.makeText(getApplicationContext(), "Please enter valid email", Toast.LENGTH_LONG).show();
+            }
+        }
+        // When any of the Edit View control left blank
+        else{
+            Toast.makeText(getApplicationContext(), "Please fill the form, don't leave any field blank", Toast.LENGTH_LONG).show();
+        }
+
+    }
+
+    /**
+     * Method that performs RESTful webservice invocations
+     *
+     * @param params
+     */
+    public void invokeWS(RequestParams params){
+        // Show Progress Dialog
+        prgDialog.show();
+        // Make RESTful webservice call using AsyncHttpClient object
+        AsyncHttpClient client = new AsyncHttpClient();
+
+        client.get("http://192.168.2.2:9999/useraccount/register/doregister",params ,new AsyncHttpResponseHandler() {
+            // When the response returned by REST has Http response code '200'
+            @Override
+            public void onSuccess(int statusCode, Header[] headers, byte[] response) {
+                // Hide Progress Dialog
+                prgDialog.hide();
+                try {
+                    String responseString = new String(response);
+                    // JSON Object
+                    JSONObject obj = new JSONObject(responseString);
+                    // When the JSON response has status boolean value assigned with true
+                    if(obj.getBoolean("status")){
+                        // Set Default Values for Edit View controls
+                        setDefaultValues();
+                        // Display successfully registered message using Toast
+                        Toast.makeText(getApplicationContext(), "You are successfully registered!", Toast.LENGTH_LONG).show();
+                    }
+                    // Else display error message
+                    else{
+                        Toast.makeText(getApplicationContext(), obj.getString("error_msg"), Toast.LENGTH_LONG).show();
+                    }
+                } catch (JSONException e) {
+                    // TODO Auto-generated catch block
+                    Toast.makeText(getApplicationContext(), "Error Occured [Server's JSON response might be invalid]!", Toast.LENGTH_LONG).show();
+                    e.printStackTrace();
+
+                }
+            }
+            // When the response returned by REST has Http response code other than '200'
+            @Override
+            public void onFailure(int statusCode, Header[] headers, byte[] responseBody, Throwable error) {
+                // Hide Progress Dialog
+                prgDialog.hide();
+                // When Http response code is '404'
+                if(statusCode == 404){
+                    Toast.makeText(getApplicationContext(), "Requested resource not found", Toast.LENGTH_LONG).show();
+                }
+                // When Http response code is '500'
+                else if(statusCode == 500){
+                    Toast.makeText(getApplicationContext(), "Something went wrong at server end", Toast.LENGTH_LONG).show();
+                }
+                // When Http response code other than 404, 500
+                else{
+                    Toast.makeText(getApplicationContext(), "Unexpected Error occcured! [Most common Error: Device might not be connected to Internet or remote server is not up and running]", Toast.LENGTH_LONG).show();
+                }
+            }
+        });
+    }
+
+    /**
+     * Method which navigates from Register Activity to Login Activity
+     */
+    public void navigatetoLoginActivity(View view){
+        Intent loginIntent = new Intent(getApplicationContext(),MainActivity.class);
+        // Clears History of Activity
+        loginIntent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+        startActivity(loginIntent);
+    }
+
+    /**
+     * Set degault values for Edit View controls
+     */
+    public void setDefaultValues(){
+        emailET.setText("");
+    }
 
 }
