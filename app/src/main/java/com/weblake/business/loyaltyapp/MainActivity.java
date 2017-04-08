3e3e0eba2c282e63da3ea1b680963a5375993566
essalhi <khalid.essalhi@gmail.com>
2017-04-08 23:51:33 +0100

    ajout de fichier .gitignore

--- app/src/main/java/com/weblake/business/loyaltyapp/MainActivity.java
+++ app/src/main/java/com/weblake/business/loyaltyapp/MainActivity.java
@@ -75,7 +75,7 @@ public class MainActivity extends AppCompatActivity
         mainActivityBTQRCode.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                
+
                 mainActivityIVQRCode.startAnimation(animationSet);
             }
         });
